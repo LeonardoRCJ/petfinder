508620280a6215a2db7a31b8ff52cd62e2d6b4ee
Leonardo Rocha Cavalcante <leonardorocha790@gmail.com>
2025-05-27 17:26:52 -0300

    feat: adding method editUser

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -34,3 +34,16 @@ exports.getUserById = async (req, res) => {
         res.status(500).json({ message: 'Erro ao buscar usuário' });
     }
 }
+
+
+exports.editUser = async (req, res) => {
+    try {
+        const user = await prisma.user.update({
+            where: {id: parseInt(req.params.id)}
+        });
+        if (!user) return res.status(404).json({message: 'Usuário não encontrado'});
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({message: 'Erro ao buscar usuário', log: err})
+    }
+}
--- src/routes/users.js
+++ src/routes/users.js
@@ -7,6 +7,7 @@ const { protect, restrictTo } = require('../middlewares/auth');
 router.get('/', protect, restrictTo('ADMIN'), usersController.getAllUsers)
 router.get('/:id', protect, usersController.getUserById)
 router.delete('/:id', protect, restrictTo('ADMIN'), usersController.deleteUser)
+router.put('/:id', protect, usersController.editUser);
 
 
 module.exports = router
